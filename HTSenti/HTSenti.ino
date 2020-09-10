0bdfa5de6ba35e0a097a936afc8c98e9c4826adb
tristan-f-cool <tristan.f.cool@gmail.com>
2020-09-09 22:34:38 -0400

    added button functionality for heat-index for MVP

--- HTSenti/HTSenti.ino
+++ HTSenti/HTSenti.ino
@@ -20,8 +20,18 @@ float diff_temperature = 0.5;
 float humidity;
 float prev_humidity;
 float diff_humidity = 0.75;
+float heat_index;
+bool Fahrenheit = false;
 bool DHTfail = false;
 bool DHTupdate = true;
+
+bool corrected_temperature;
+
+//~~~~~~ BUTTON ~~~~~~~~~~~
+byte Button = 0;
+byte prev_Button = 0;
+long button_time = 0;
+long button_debounce = 50;
 
 /*################## SETUP #############################################*/
 
@@ -34,6 +44,9 @@ void setup()
 
   //initialize DHT22
   dht.begin();
+
+  //setup push-button
+  pinMode(pinButton, INPUT);
   
 }//END: setup
 
@@ -41,13 +54,46 @@ void setup()
 
 void loop() 
 {
-  delay(2000);
-  readDHT();    // read DHT22
-  printDHT();   // print DHT22
-
+  if(DEBUG) //DEBUG MODE
+  {
+    Serial.println(F("$$$$$$$$$$$$$$ DEBUG $$$$$$$$$$$$$$$$$$$$$"));
+    debug();
+  }
+  
+  else      //NORMAL OPERATION
+  {
+    checkButton();  // check push-button
+    delay(2000);    // delay for sensor polling
+    readDHT();      // read DHT22
+    printDHT();     // print DHT22
+  }
 }//END: loop
 
 /*################## FUNCTIONS ########################################*/
+void debug()
+{
+
+}//END: debug
+
+void checkButton()
+{
+  Button = digitalRead(pinButton);
+  //Serial.println(Button);
+  if(Button && !prev_Button && (millis() - button_time > button_debounce))
+  {
+    if(corrected_temperature)
+    {
+      Serial.println(F("Heat Index: OFF"));
+      corrected_temperature = false;
+    }
+    else
+    {
+      Serial.println(F("Heat Index: ON"));
+      corrected_temperature = true;
+    }
+  }
+  
+}//END: checkButton
 
 void readDHT()
 {
@@ -56,22 +102,28 @@ void readDHT()
   prev_temperature = temperature;      //store last temperatue reading
   temperature = dht.readTemperature(); //record new temperature value
 
+/*
   //check for significant change in temperature or humidity
   if((abs(prev_humidity - humidity) > diff_humidity) || (abs(prev_temperature - temperature) > diff_temperature))
     DHTupdate = true;
   else
     DHTupdate = false;
-
-   // Check if any reads failed and exit early (to try again).
+*/
+  // Check if any reads failed and exit early (to try again).
   if (isnan(humidity)|| isnan(temperature)) 
   {
     DHTfail = true;
-    return; //retry sensor
+    return; //break
   }
   DHTfail = false;
 
+  //TODO - check for max and min values
+  
   prev_temperature = temperature;
   prev_humidity = humidity;
+
+  // calculate heat index
+  heat_index = dht.computeHeatIndex(temperature, humidity, Fahrenheit);
   
 }//END: readDHT
 
@@ -88,6 +140,11 @@ void printDHT()
       Serial.println(humidity);
       Serial.print(F("Temperature C: "));
       Serial.println(temperature);
+      if(corrected_temperature)
+      {
+        Serial.print(F("Heat Index C: "));
+        Serial.println(heat_index);
+      }
       Serial.println(F("----------------------------------------"));
   }
   
--- HTSenti/defs.h
+++ HTSenti/defs.h
@@ -5,6 +5,16 @@
  *  defs.h
  ************************************************************************/
 
+#define DEBUG 0         // 1 = Debug mode ; 0 = Normal mode
+
 //~~~~~~~~~~~~ DHT22 Temperature Humidity Sensor ~~~~~~~~~~~~
-#define pinDHT 2
+#define pinDHT 2        // Digital pin D2
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+#define MAX_TEMPERATURE 85
+#define MIN_TEMPERATURE -25
+#define MAX_HUMIDTY 80
+#define MIN_HUMIDITY 0
+
+//~~~~~~~~~~~~ Push Button ~~~~~~~~~~~~
+#define pinButton A5    // Analog pin A5
